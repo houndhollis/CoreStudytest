f907951a332c0e48a34c37645860fea7598d078d
Hyunjun <yhj0439@nate.com>
2022-06-26 16:59:33 +0900

    findID 완성

--- controller/index.js
+++ controller/index.js
@@ -3,10 +3,22 @@ const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
   findAll: (req, res) => {
     // TODO: 모든 discussions 목록을 응답합니다.
+    res.status(200).send(discussionsData)
   },
     
   findById: (req, res) => { 
-  
+    let list = discussionsData
+
+    list = list.filter(el => {
+      return el.id === Number(req.params.id)
+    })
+
+    if(list.length > 0){
+      return res.status(200).json(list)
+    }
+    else{
+     throw res.status(404)
+    }
   },
   createOne: (req, res) => {
 
